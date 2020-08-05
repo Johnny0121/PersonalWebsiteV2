a1572075ed10f7fdf7d63bf6a5c5f59f108623bd
Johnny Le <contact.lejohnny@gmail.com>
2020-08-05 16:14:27 +0100

    Content changes

---
+++ PersonalWebsiteV2/obj/Release/netcoreapp3.1/win-x86/PubTmp/Out/wwwroot/js/modules/Avatar.js
@@ -0,0 +1,21 @@
+﻿var avatarModule = function () {
+    var avatars = $('.avatar');
+
+    if (avatars.length == 1) {
+        setBackgroundImage(avatars);
+    }
+    else if (avatars.length > 1) {
+        $(avatars).each(function () {
+            setBackgroundImage(this);
+        });
+    }
+
+    function setBackgroundImage(avatarElem) {
+        var childImage = $(avatarElem).children('img').first();
+
+        if (childImage) {
+            var resourceLocation = $(childImage).attr('src') || '';
+            $(avatarElem).css('background-image', `url('${resourceLocation}')`)
+        }
+    }
+}();
