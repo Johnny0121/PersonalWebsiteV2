cb58d9ffbd1da3fb5dce0bddaac117e772211b22
Johnny Le <johnnyxdblack@gmail.com>
2020-10-22 23:18:11 +0100

    Content changes

---
+++ PersonalWebsiteV2/Controllers/AcademicsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebsiteV2.Controllers
+{
+    public class AcademicsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Year4()
+        {
+            return View();
+        }
+    }
+}
